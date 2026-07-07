1365a70f65caec50f09fea8f7ca2aa3b7ca4394f
Divyanshu Saxena <saxenadivyanshu4518@gmail.com>
2026-07-07 13:11:09 +0530

    Initial version

---
+++ src/Calculator_Machine/App.java
@@ -0,0 +1,8 @@
+package Calculator_Machine;
+
+public class App {
+    static void main(String[] args) {
+        Calculator calculator = new Calculator();
+    }
+}
+
---
+++ src/Java_Swing/JFrame_Demo_WithLogo.java
@@ -0,0 +1,31 @@
+package Java_Swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JFrame_Demo_WithLogo {
+    static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        frame.setTitle("My GUI JFrame");
+
+        // frame.setSize(400,400);
+
+        frame.setBounds(400,200,400,400);
+
+        frame.getContentPane().setBackground(Color.BLUE);
+
+        frame.setMaximumSize(new Dimension(100,100));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        ImageIcon logo = new ImageIcon("logo.jpeg");
+        frame.setIconImage(logo.getImage());
+
+
+        frame.setVisible(true);
+    }
+}
+
